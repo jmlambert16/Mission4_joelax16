b1f24b58a02e960217aa61f564f291493bd95021
Joe Lambert <jmlambert58@gmail.com>
2023-02-06 09:55:45 -0700

    Mission5

---
+++ Mission4(1)/Controllers/blah.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Mission4_1_.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+
+namespace Mission4_1_.Controllers
+{
+    public class blah : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult calc()
+        {
+            return (View());
+        }
+        
+        [HttpPost]
+        public IActionResult calc(calc_model model)
+        {
+            return View();
+        }
+    }
+}
---
+++ Mission4(1)/Models/calc_model.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Mission4_1_.Models
+{
+    public class calc_model
+    {
+        [Required]
+        [Range(0, 100)]
+        public int assignment { get; set; }
+        public int group { get; set; }
+        public int quiz { get; set; }
+        public int midterm { get; set; }
+        public int final { get; set; }
+        public int intex { get; set; }
+
+
+
+    }
+}
